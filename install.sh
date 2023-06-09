78ef34e04565eb0dcdb3a2d77c01343d6b3a8fa8
Christopher Adams <adamscj14@gmail.com>
2023-06-09 10:50:56 -0400

    removed setuptools (replaced with pip in env file) and used bash operator to ensure all run successfully

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-conda env create --name baymer --file baymer_conda_env.yaml
-conda activate baymer
-python setup.py install
+conda env create --name baymer --file baymer_conda_env.yaml && conda activate baymer
